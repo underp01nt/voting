726a65fe1b640bf9148b194146c2a7b10767d78c
underp01nt <ernestothai123@gmail.com>
2026-04-28 21:19:05 -0500

    implement upload route

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from ranked_pairs import ranked_pairs
 
-from fastapi import FastAPI, UploadFile, File, Request, Form
+from fastapi import FastAPI, UploadFile, File, Request, Form, HTTPException
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
+
+from io import StringIO
+import csv, time
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -13,10 +16,42 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 def validate_client_csv_input(client_data: list[list[str]]) -> list[list[str]]:
     raise NotImplementedError()
 
-# TODO: implement the upload route to consume client input
 @app.post("/upload")
-async def upload(file: UploadFile=File(...), algorithms: list[str]=Form(...)):
-    raise NotImplementedError()
+async def upload(request:Request, file: UploadFile=File(...), algorithms: list[str]=Form(...)):
+
+    # make text reader iterable per row
+    def parse_csv(text: str) -> list[list[str]]:
+        reader = csv.reader(StringIO(text))
+        return [row for row in reader if row]
+
+    try:
+        data = await file.read()
+        text = data.decode("utf-8")  # convert file content to text
+
+        votes = parse_csv(text) #; validate_votes(votes)
+        candidates = list(set(votes[0]))
+
+        results = {}
+        if "ranked_pairs" in algorithms:
+            start = time.time()
+            ranked_pairs_result = ranked_pairs(candidates, votes)
+            end = time.time()
+
+            results["ranked_pairs"] = {
+                "ranking": ranked_pairs_result,
+                "duration": end - start,
+            }
+
+        # if "proposal_method" in algorithms: 
+        #   ...
+
+    
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Bad data read: {str(e)}")
+
+    return {"algorithms_used": algorithms,
+        "candidates": candidates,
+        "experiments": results,}
 
 @app.get("/", response_class=HTMLResponse)
 def landing(request: Request):

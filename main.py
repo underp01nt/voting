1296e29b136a35e60fa9e15c28cdce42205ce264
underp01nt <ernestothai123@gmail.com>
2026-04-28 20:03:17 -0500

    fastapi setup

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
 from ranked_pairs import ranked_pairs
 
+from fastapi import FastAPI, UploadFile, File, Request, Form
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# TODO: verify client input, check for null entries, e.t.c.
+def validate_client_csv_input(client_data: list[list[str]]) -> list[list[str]]:
+    raise NotImplementedError()
+
+# TODO: implement the upload route to consume client input
+@app.post("/upload")
+async def upload(file: UploadFile=File(...), algorithms: list[str]=Form(...)):
+    raise NotImplementedError()
+
+@app.get("/", response_class=HTMLResponse)
+def landing(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+    )

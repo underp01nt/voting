362aed51a3943107a078e0b64acb095ba1404583
underp01nt <ernestothai123@gmail.com>
2026-04-29 17:38:34 -0500

    build heatmap figure

--- main.py
+++ main.py
@@ -18,6 +18,23 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 def validate_client_csv_input(client_data: list[list[str]]) -> list[list[str]]:
     raise NotImplementedError()
 
+def build_heat_map(candidates: list[str], votes: list[list[str]]) -> go.Figure:
+    pairs = {a: {b: 0 for b in candidates} for a in candidates}
+    for vote in votes:
+        for i in range(len(vote)):  #  i > j, get number of voters preferred (or after) candidate i
+            for j in range(i + 1, len(vote)):
+                pairs[vote[i]][vote[j]] += 1
+
+    # build matrix based on pairwise match-up scores
+    mat = [[pairs[a][b] for b in candidates] for a in candidates]
+
+    heat = go.Figure(data=go.Heatmap(z=mat, x=candidates, y=candidates, colorscale="Blues", zmin=0))
+    heat.update_layout(
+        title={"text": "Pairwise Comparisons (Vertical vs. Horizontal)", "x": 0.5}, 
+        yaxis=dict(scaleanchor="x"), width=500, height=500,
+    )
+    return heat
+
 def build_rank_table(votes: list[str]) -> go.Figure:
     num_candidates = len(votes)
     rank_indices = list(range(1, num_candidates + 1))
@@ -55,15 +72,18 @@ async def upload(request: Request, file: UploadFile=File(...), algorithms: Optio
             end = time.time()
 
             fig = build_rank_table(ranked_pairs_result)
+            heat = build_heat_map(candidates, votes)
+
             results["Ranked pairs"] = {
                 # "ranking": ranked_pairs_result,
                 "duration": end - start,
                 "fig": fig.to_html(full_html=False, config={"responsive": True}),
+                "heat": heat.to_html(full_html=False, config={"responsive": True})
             }
             
     
     except Exception as e:
-        raise HTTPException(status_code=400, detail=f"Bad data read: {e}")
+        raise HTTPException(status_code=400, detail=f"Exception occurred: {e}")
 
     return templates.TemplateResponse(
         request=request,
